f2522578c3cf14fa2e0393e0bd02855bc3d48feb
onii7z <guillaumehoulle@hotmail.fr>
2022-02-07 09:37:55 +0100

    Onii

--- src/components/modules/lexique/Lexique.vue
+++ src/components/modules/lexique/Lexique.vue
@@ -1,3 +1,42 @@
 <template>
-    <div>lexique single</div>
+    <div>
+        <!-- <div v-for="tuto in listeTuto" :key="tuto.id">
+            <img :src="tuto.image" alt="">
+            <h3>{{tuto.titre}}</h3>
+            <h5>De : </h5> <p>PierpolJak</p>
+            <h5>Note</h5> <p>5/5</p>
+            <router-link :to="{ name: 'Module_Tuto', params : {id : tuto.id}}">
+                <button  type="submit" >Voir le tuto</button>
+            </router-link>
+        </div> -->
+        test
+    </div>
 </template>
+
+<script>
+  import param from '@/param/param'
+
+    export default {
+        name: "Lexique",
+        data() {
+            return {
+                listeTuto : [],
+            }
+        },
+        // created() {
+        //     axios({
+        //         method: 'get',
+        //         url: param.hostTuto + '/listeTuto',
+        //     }).then(function(response) {
+        //         console.log('test tuto', response);
+        //         this.listeTuto = response.data;
+        //         console.log('liste tuto', this.listeTuto);
+        //     }.bind(this))
+        //     .catch(error => console.log(error))
+        // },
+    };
+</script>
+
+<style scoped>
+
+</style>
--- src/components/modules/lexique/Lexique_Sommaire.vue
+++ src/components/modules/lexique/Lexique_Sommaire.vue
@@ -1,5 +1,40 @@
 <template>
     <div>
-        sommaire lexique
+        <div v-for="lexique in lexiques" :key="lexique.id">
+            <h3>{{lexique.title.rendered}}</h3>
+            <p>{{lexique.acf.description}}</p>
+            <p>{{lexique.acf.lien_source}}</p>
+            <!-- <router-link :to="{ name: 'Module_Tuto', params : {id : tuto.id}}">
+                <button  type="submit" >Voir le tuto</button>
+            </router-link> -->
+        </div>
     </div>
 </template>
+
+<script>
+  import param from '@/param/param'
+
+    export default {
+        name: "Lexique_Sommaire",
+        data() {
+            return {
+                lexiques : [],
+            }
+        },
+        created() {
+            axios({
+                method: 'get',
+                url: param.host + 'lexique',
+            }).then(function(response) {
+                console.log('test lexique', response);
+                this.lexiques = response.data;
+                console.log('liste lexique', this.lexiques);
+            }.bind(this))
+            .catch(error => console.log(error))
+        },
+    };
+</script>
+
+<style scoped>
+
+</style>
