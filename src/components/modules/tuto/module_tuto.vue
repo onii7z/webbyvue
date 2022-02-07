bf295f7a2ad66f8bcd647902e01caa279bf4408d
onii7z <guillaumehoulle@hotmail.fr>
2022-02-07 08:42:50 +0100

    Onii

--- src/components/modules/tuto/module_tuto.vue
+++ src/components/modules/tuto/module_tuto.vue
@@ -1,35 +1,33 @@
 <template>
     <div>
-        single tuto
-    </div>
-</template>
-
-    <!-- <div>
-        <div v-for="tuto in listeTuto" :key="tuto.id">
+        <div v-for="tutos in tuto" :key="tutos.id">
             <h3>{{tuto.titre}}</h3>
             <h5>Catégorie</h5>
-            <p v-for="cat in tuto.categorie" :key="cat.id">
+            <p v-for="cat in tutos.categorie" :key="cat.id">
                 {{cat.nom}}
             </p>
             <h5>Niveau</h5>
-            <p v-for="niv in tuto.niveau" :key="niv.id"> 
+            <p v-for="niv in tutos.niveau" :key="niv.id"> 
                 {{ niv.nom }} 
             </p>
-            <h5>Résumé </h5><p>{{ tuto.resume }}</p>
+            <h5>Résumé </h5><p>{{ tutos.resume }}</p>
             <h5>Format</h5>
-            <p v-for="format in tuto.type" :key="format.id">
+            <p v-for="format in tutos.type" :key="format.id">
                 {{format.nom}}
             </p>
-            <h5>Lien</h5><a>{{tuto.lien}}</a>
+            <h5>Lien</h5><a>{{tutos.lien}}</a>
             <h5>Langue</h5>
-            <p v-for="lang in tuto.langue" :key="lang.id">
+            <p v-for="lang in tutos.langue" :key="lang.id">
                 {{lang.nom}}
             </p>
             <h5>Note</h5>
-            <img :src="tuto.image" alt="">
+            <img :src="tutos.image" alt="">
             <h3>Commentaires : </h3><p></p>
         </div>
-    </div> -->
+    </div>
+</template>
+
+    
 
 <script>
     import param from '@/param/param'
