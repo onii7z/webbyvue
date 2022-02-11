8b69ece08f6f5baa4b489fa56d54c114184cfec9
guillaume Houllé <guillaumehoulle@gmail.com>
2022-02-11 14:27:15 +0100

    Onii

--- src/components/pages/Search.vue
+++ src/components/pages/Search.vue
@@ -1,114 +1,174 @@
 <template>
-    <div>
-        <input v-model="query" type="search" placeholder="Recherche de tuto">
-        <div class="grid">
-        <p v-if="recherche.length == 0">Aucun résultat</p>
-            <div class="cards" v-for="tuto in recherche" :key="tuto.id">
-                <img class="cards__img" :src="tuto.image" alt="">
-                <h3 class="cards__titre">{{tuto.titre}}</h3>
-                <h5 class="cards__desc">Note : {{tuto.note}}/5</h5>
-                <router-link class="cards__lien" :to="{ name: 'Module_Tuto', params : {id : tuto.id}}">
-                    <p class="cards__sstitre">Voir le tuto</p>
-                </router-link>
-            </div>
-        </div>
+  <div>
+    <input v-model="query" placeholder="Recherche de tuto" />
+    <select v-model="query">
+      <option selected disabled>Catégories</option>
+      <option v-for="cat in cats" :key="cat.id" :value="cat.name">
+        {{ cat.name }}
+      </option>
+    </select>
+    <select v-model="query">
+      <option selected disabled>Niveau</option>
+      <option v-for="niv in nivs" :key="niv.id" :value="niv.name">
+        {{ niv.name }}
+      </option>
+    </select>
+    <select v-model="query">
+      <option selected disabled>Format</option>
+      <option v-for="format in formats" :key="format.id" :value="format.name">
+        {{ format.name }}
+      </option>
+    </select>
+    <select v-model="query">
+      <option selected disabled>Langue</option>
+      <option v-for="lang in langs" :key="lang.id" :value="lang.name">
+        {{ lang.name }}
+      </option>
+    </select>
+
+    <div class="grid">
+      <p v-if="recherche.length == 0">Aucun résultat</p>
+      <div class="cards" v-for="tuto in recherche" :key="tuto.id">
+        <img class="cards__img" :src="tuto.image" alt="" />
+        <h3 class="cards__titre">{{ tuto.titre }}</h3>
+        <h5 class="cards__desc">Note : {{ tuto.note }}/5</h5>
+        <router-link
+          class="cards__lien"
+          :to="{ name: 'Module_Tuto', params: { id: tuto.id } }"
+        >
+          <p class="cards__sstitre">Voir le tuto</p>
+        </router-link>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-// ne fonctione pas
-  import param from '@/param/param'
+import param from "@/param/param";
 
-    export default {
-      name: "search",
-      data() {
-          return {
-              search : [],
-              query: "",
-          }
-      },
-      computed:{  
-            // FONCTIONNE mais pas comme je veux
-            recherche: function(){
-                var query = this.query;
-                return this.search.filter(function(tuto){
-                    console.log('titre', tuto.titre);
-                    return tuto.titre.toLowerCase().includes(query.toLowerCase());
-                })
-            },
-
-
-            // recherche: function(){
-            //     var query = this.query;
-            //     return this.search.filter(function(tuto){
-            //         console.log('titre', tuto.nom);
-            //         return tuto.titre.toLowerCase().includes(query.toLowerCase());
-            //     })
-            // },
-            // bySpecialite: function(spe){
-            //     return this.search.filter(function(tuto){
-            //         return tuto.categorie.nom === spe;
-            //     })
-            // }
-            // recherche: function(){
-            //     // var query = this.query;
-            //    const newArray = this.search.filter((value, index) => {
-            //         console.log('value', value);
-            //         console.log('index', index);
-            // // if (value > 5) return true;
-            // });
-            // },
-            // recherche: function(){
-            //     var query = this.query;
-            //     let test = this.search.forEach(function(tuto){
-            //         console.log('Data1', tuto);
-
-            //         return tuto.titre.includes(query);
-            //     })
-            //     return test;
-            // },
-
-        // recherche: function(){
-        //       var query = this.query;
-        //       var longeur = this.search.length;
-        //       console.log('longueur', longeur);
-        //       let test = this.search.filter(function(tuto){
-
-        //         // console.log('gjeir',tuto.categorie);
-        //         // for (const cat in tuto.categorie) {
-        //         //     console.log('ta mere',cat);
-        //         //     // for (const catName in cat) {
-        //         //     // console.log(catName);
-        //         //     // }
-        //         // }
-
-        //         // return tuto.titre.includes(query);
-        //         for (let i =0; i < longeur; i++){
-        //             // console.log('blgm,d');
-        //             console.log('tddt',tuto.categorie);
-                    
-        //             // return test;
-        //             // if (tuto.categorie[i] == query) {
-        //             //     // return tuto.titre;
-        //             //     console.log('blopppp');
-        //             // }
-        //         }
-
-        //       });
-        //       return test;
-        //   },
-
-      },
-      created() {
-          axios({
-              method: 'get',
-              url: param.hostTuto + '/listeTuto',
-          }).then(function(response) {
-              console.log('Json réponse', response);
-              this.search = response.data;
-              console.log('Json search', this.search);
-          }.bind(this))
-          .catch(error => console.log(error))
-        },
+export default {
+  name: "search",
+  data() {
+    return {
+      search: [],
+      query: "",
+      cats: [],
+      nivs: [],
+      langs: [],
+      formats: [],
     };
+  },
+  computed: {
+    recherche: function () {
+      var query = this.query;
+      var res;
+      return this.search.filter(function (tuto) {
+        for (let i = 0; i < tuto.categorie.length; i++) {
+          // console.log('titre', tuto.categorie[i].nom);
+          if (query.toLowerCase() == tuto.categorie[i].nom.toLowerCase()) {
+            res = tuto.categorie[i].nom;
+            return res.toLowerCase().includes(query.toLowerCase());
+          }
+        }
+        for (let i = 0; i < tuto.tag.length; i++) {
+          // console.log('titre', tuto.categorie[i].nom);
+          if (query.toLowerCase() == tuto.tag[i].nom.toLowerCase()) {
+            res = tuto.tag[i].nom;
+            return res.toLowerCase().includes(query.toLowerCase());
+          }
+        }
+        for (let i = 0; i < tuto.langue.length; i++) {
+          // console.log('titre', tuto.categorie[i].nom);
+          if (query.toLowerCase() == tuto.langue[i].nom.toLowerCase()) {
+            res = tuto.langue[i].nom;
+            return res.toLowerCase().includes(query.toLowerCase());
+          }
+        }
+        for (let i = 0; i < tuto.niveau.length; i++) {
+          // console.log('titre', tuto.categorie[i].nom);
+          if (query.toLowerCase() == tuto.niveau[i].nom.toLowerCase()) {
+            res = tuto.niveau[i].nom;
+            return res.toLowerCase().includes(query.toLowerCase());
+          }
+        }
+        for (let i = 0; i < tuto.type.length; i++) {
+          // console.log('titre', tuto.categorie[i].nom);
+          if (query.toLowerCase() == tuto.type[i].nom.toLowerCase()) {
+            res = tuto.type[i].nom;
+            return res.toLowerCase().includes(query.toLowerCase());
+          }
+        }
+        if (query.toLowerCase() == tuto.resume.toLowerCase()) {
+          return tuto.resume.toLowerCase().includes(query.toLowerCase());
+        }
+        if (query.toLowerCase() == tuto.titre.toLowerCase()) {
+          return tuto.titre.toLowerCase().includes(query.toLowerCase());
+        }
+        return tuto.titre.toLowerCase().includes(query.toLowerCase());
+      });
+    },
+  },
+  created() {
+    axios({
+      method: "get",
+      url: param.hostTuto + "/listeTuto",
+    })
+      .then(
+        function (response) {
+          //   console.log('Json réponse', response);
+          this.search = response.data;
+          console.log("Json search", this.search);
+        }.bind(this)
+      )
+      .catch((error) => console.log(error));
+    axios({
+      method: "get",
+      url: param.host + "cat_tuto",
+    })
+      .then(
+        function (response) {
+          //   console.log('Json réponse', response);
+          this.cats = response.data;
+          console.log("Json cat", this.cats);
+        }.bind(this)
+      )
+      .catch((error) => console.log(error));
+    axios({
+      method: "get",
+      url: param.host + "niv_tuto",
+    })
+      .then(
+        function (response) {
+          //   console.log('Json réponse', response);
+          this.nivs = response.data;
+          console.log("Json niv", this.nivs);
+        }.bind(this)
+      )
+      .catch((error) => console.log(error));
+    axios({
+      method: "get",
+      url: param.host + "lang_tuto",
+    })
+      .then(
+        function (response) {
+          //   console.log('Json réponse', response);
+          this.langs = response.data;
+          console.log("Json langs", this.langs);
+        }.bind(this)
+      )
+      .catch((error) => console.log(error));
+    axios({
+      method: "get",
+      url: param.host + "type_tuto",
+    })
+      .then(
+        function (response) {
+          //   console.log('Json réponse', response);
+          this.formats = response.data;
+          console.log("Json formats", this.formats);
+        }.bind(this)
+      )
+      .catch((error) => console.log(error));
+  },
+};
 </script>
