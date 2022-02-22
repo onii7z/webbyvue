ab1f6f9453d64af059e622ec6ebe03a7929bc1c8
guillaume Houllé <guillaumehoulle@gmail.com>
2022-02-22 13:01:16 +0100

    contact

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,27 +1,28 @@
 <template>
-  <div>
-    <h2>Conctactez nous</h2>
-    <form>
-      <input
-        required
-        name="name"
-        placeholder="Name"
-        type="text"
-        autocomplete="off"
-      />
-      <input
-        required
-        name="email"
-        placeholder="E-mail"
-        type="email"
-        autocomplete="off"
-      />
-      <textarea
-        name="message"
-        rows="4"
-        placeholder="Message"
-      ></textarea>
-      <button class="button">Send</button>
-    </form>
-  </div>
+  <form ref="form" @submit.prevent="sendEmail">
+    <label>Votre Nom/Pseudo</label>
+    <input type="text" name="user_name">
+    <label> Votre Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Envoyer">
+  </form>
 </template>
+
+<script>
+import emailjs from '@emailjs/browser';
+
+export default {
+  methods: {
+    sendEmail() {
+      emailjs.sendForm('service_yswjkwh', 'template_wv5kapz', this.$refs.form, 'user_RHZXIbvoJrtTEIdFa28pt')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+        }, (error) => {
+            console.log('FAILED...', error.text);
+        });
+    }
+  }
+}
+</script>
