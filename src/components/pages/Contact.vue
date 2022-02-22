c8fe25509d29e494a3edc022d4621ecb0b480dcc
guillaume Houllé <guillaumehoulle@gmail.com>
2022-02-22 11:21:10 +0100

    Onii

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        contact
-    </div>
+  <div>
+    <h2>Conctactez nous</h2>
+    <form>
+      <input
+        required
+        name="name"
+        placeholder="Name"
+        type="text"
+        autocomplete="off"
+      />
+      <input
+        required
+        name="email"
+        placeholder="E-mail"
+        type="email"
+        autocomplete="off"
+      />
+      <textarea
+        name="message"
+        rows="4"
+        placeholder="Message"
+      ></textarea>
+      <button class="button">Send</button>
+    </form>
+  </div>
 </template>
