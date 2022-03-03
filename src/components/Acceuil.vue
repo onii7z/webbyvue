03fe9872474c6b50653d05bcf7b5bd57e16e28be
Amandine <amandine.lec@hotmail.fr>
2022-03-03 15:34:17 +0100

    Accueil

--- src/components/Acceuil.vue
+++ src/components/Acceuil.vue
@@ -85,8 +85,10 @@
             <img class="section__img" src="../assets/imgs/acceuil1.jpg" alt="#">
             <h2 class="section__title">Qui sommes-nous ? </h2>
             <p class="section__desc">Webby les bons tuyaux est un site web communautaire, mis à jour par la communauté. Créé par des étudiants de la grande école du numérique, il a pour objectif le partage des meilleurs tutoriels pour aider les étudiants et les internautes grâce à un système de notation.</p>
-            <div class="section__link">
-              <router-link to="/quisommes" class="section__lien"> En savoir plus ... </router-link>
+            <div class="section__position">
+              <div class="section__link">
+                <router-link to="/quisommes" class="section__lien"> En savoir plus ... </router-link>
+              </div>
             </div>
           </section>
 
@@ -94,8 +96,10 @@
             <img class="section__img --tuto__img" src="../assets/imgs/acceuil2.jpg" alt="#">
             <h2 class="section__title --tuto__title">Partagez et notez les tutos</h2>
             <p class="section__desc --tuto__desc">Les étudiants de la grande école du numérique et du département métiers et multimédias de l’internet de l’université de Montbéliard vous partagent les tutoriels leur ayant été utiles. A vous de noter ces tutoriels pour aider la communauté à choisir celui qui pourra l’aider dans son projet. </p>
-            <div class="section__link --tuto__link">
-              <router-link to="/tuto" class="section__lien --tuto__lien"> Découvrez nos tutos ... </router-link>
+            <div class="section__position --tuto__position">
+              <div class="section__link --tuto__link">
+                <router-link to="/tuto" class="section__lien --tuto__lien"> Découvrez nos tutos ... </router-link>
+              </div>
             </div>
           </section>
 
@@ -103,9 +107,12 @@
             <img class="section__img" src="../assets/imgs/acceuil3.jpg" alt="#">
             <h2 class="section__title">Suivez notre parcours initiatique</h2>
             <p class="section__desc">Si vous souhaitez apprendre les bases du développement mobile tout en étant guidé dans votre progression, notre parcours initiatique est fait pour vous : lexique de base, apprentissage des premiers langages et design sont au programme. </p>
-            <div class="section__link">
-              <router-link to="/presentation_parcours" class="section__lien"> Commencez par ici ! </router-link>
+            <div class="section__position">
+              <div class="section__link">
+                <router-link to="/presentation_parcours" class="section__lien"> Commencez par ici ! </router-link>
+              </div>
             </div>
+            
 
           </section>
         </div>
