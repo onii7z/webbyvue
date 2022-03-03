5e36fde24d88b6fe60b0f43d78019d83204ab4d9
guillaume Houllé <guillaumehoulle@gmail.com>
2022-03-03 12:17:46 +0100

    Onii

--- src/components/modules/tuto/module_tuto.vue
+++ src/components/modules/tuto/module_tuto.vue
@@ -24,9 +24,21 @@
             </p>
             <h5 class="tuto__sstitre">Note</h5>
             <p class="tuto__desc">{{tutos.note}}/5</p>
+            <!-- <input type="hidden" :placeholder="tutos.nbnote"> -->
             <h5 class="tuto__sstitre">Notez moi</h5>
-            <p class="tuto__desc">note/5</p>
-            <button class="tuto__noter">Noter</button>
+            <form @submit="noter" >
+                <p class="tuto__desc">
+                    <select v-model="notes">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    /5
+                </p>
+                <button type="submit" class="tuto__noter">Noter</button>
+            </form>
             <img class="tuto__image" :src="tutos.image" alt="">
             <h3 class="tuto__titre">Commentaires : </h3><hr class="tuto__barre">
             <form @submit="creer" class="tuto__comments comments">
@@ -47,6 +59,7 @@
 <script>
     import param from '@/param/param';
     import appService from '@/services/appService';
+    // import apiFetch from '@wordpress/api-fetch';
 
 
     export default {
@@ -59,6 +72,7 @@
                 commentaires : {
                     contenu:null
                 },
+                notes : "",
                 utilisateur: null
             }
         },
@@ -102,6 +116,102 @@
                     // this.name.$forceUpdate();
                     this.$router.go();
                     // $router.go();
+                }.bind(this))
+                .catch(error => console.log(error))
+            },
+            noter: function() {
+
+                console.log("tuto notes", this.tuto[0].note);
+                console.log("tuto nbnotes", this.tuto[0].nbnote);
+                console.log("notes", this.notes);
+                // var n1 = parseInt(this.tuto[0].note);
+                // var n2 = parseInt(this.tuto[0].nbnote);
+                // var n3 = parseInt(this.notes);
+                // var test = n1 + n3;
+                // var test = (n1, n3 ) => n1 + n3;
+                // console.log('rrrrrrr', test);
+
+                // limite à 2 chiffres après la virgule
+                function nombre(x) {
+                    return Number.parseFloat(x).toFixed(2);
+                }
+                // création des variables pour l'opération c'est plus lisible
+                var temp1 = nombre(this.tuto[0].note);
+                console.log('temp1 note globales', temp1);
+                var temp2 = parseInt(this.tuto[0].nbnote);
+                console.log('temp2 nb notes', temp2);
+                var temp3 = parseInt(this.notes);
+                console.log('temp3 note', temp3);
+
+                // Addition pour augmenter le compteur de vote à chaque vote
+                var newNbNote = temp2 +1;
+                console.log('nouvelle nb note', newNbNote);
+
+                // Début du calcul de la nouvelle note
+                // on multiplie la note existante par le nombre de note actuelle 
+                var mult = temp1 * temp2;
+                console.log('resul mult', mult);
+                
+                // On addicte le résultat précedent avec la note mise par l'utilisateur
+                var add = mult + temp3;
+                console.log('resul add', add);
+
+                // On divise le résultat précedent par le nouveau nombre de vote
+                var div = add / newNbNote;
+                console.log('resul div', div);
+
+                // Pour finir on le passe dans la fonctionne pour éviter qu'il y a plus de 2 chiffres après la virgule.
+                var newNote = nombre(div);
+                console.log('nouvele note', newNote);
+
+                
+
+
+
+
+
+
+                // var newNote = ((parseInt(this.tuto[0].note) * parseInt(this.tuto[0].nbnote))+parseInt(this.notes))/(parseInt(newNbNote));
+                // console.log('parse', newNote);
+                // var newNote1 = (this.tuto[0].note*this.tuto[0].nbnote)+this.notes;
+                // var newNote2 = (this.tuto[0].nbnote++);
+                // var newNote = newNote1/newNote2;
+
+                
+
+                // var newNoteForm = financial(newNote);
+
+
+                
+                // console.log("nouvelle note :", newNote);
+                // console.log("nouvelle nbnote :", newNbNote);
+
+
+                this.utilisateur = appService.getLocal();
+                console.log('utilisateur', this.utilisateur);
+                var idpost = this.$route.params.id;
+                console.log('id', idpost);
+
+                let headers = {
+                    'Authorization' : 'Bearer ' + this.utilisateur.token
+                }
+
+                axios({
+                    method: 'post',
+                    data: {
+                        status : "publish",
+                        fields : {
+                            note_globale : newNote,
+                            nb_note : newNbNote,
+                        }
+                    },
+                    url: param.host +'tuto/'+idpost,
+                    headers: headers
+                }).then(function(response) {
+                    console.log('Retour Créa note', response);
+                    alert("Note prise en compte");
+                    this.$router.go();
+
                 }.bind(this))
                 .catch(error => console.log(error))
             }
